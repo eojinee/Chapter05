6af24c8b20ed417b4c7f987ba0a50fc292853071
어진 김 <k91897599@gmail.com>
2023-03-10 15:29:28 +0900

    202303101529.ifelse

--- IfElse/MainApp.cs
+++ IfElse/MainApp.cs
@@ -7,7 +7,34 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
-           //dfdfsddd
+            Console.WriteLine("숫자를 입력하세요.");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+            {
+                Console.WriteLine("음수");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("양수");
+            }            
+            else
+            {
+                Console.WriteLine("0");
+            }
+                
+            if(number%2==0)
+            {
+                Console.WriteLine("짝수");
+            }          
+            else
+            {
+                Console.WriteLine("홀수");
+            }
+           
+            Console.WriteLine(number % 2 == 0 ?"짝수" : "홀수");
 
         }
     }

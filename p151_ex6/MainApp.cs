752660d0b07b7b108c387f51ab42af020a3ba1d0
어진 김 <k91897599@gmail.com>
2023-03-15 23:07:57 +0900

    03152257 if6번

--- p151_ex6/MainApp.cs
+++ p151_ex6/MainApp.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace p151_ex6
 {
@@ -7,23 +10,50 @@ namespace p151_ex6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("중간고사 점수 합격 여부 프로그램.");
+            Console.WriteLine("중간고사 점수 합격여부 프로그램.");
             Console.Write("현재 본인의 학년을 숫자만 입력하세요.(ex.2):");
+            string a = Console.ReadLine();
+            int grade = Int32.Parse(a);
+
+
+            Console.Write("현재 본인의 점수를 입력해 주세요.(ex.85)");
+            string b = Console.ReadLine();
+            int score = Int32.Parse(b);
+
+            if (score > 0 && score <= 100)
+            {
+                if (grade == 4)
+                {
+                    if (score >= 70)
+                    {
+                        Console.WriteLine($"입력하신 점수 {score}은(는) 합격입니다.");
+                    }
+                    else if (score < 70)
+                    {
+                        Console.WriteLine($"입력하신 점수 {score}은(는) 불합격입니다.");
+                    }
+
+                }
+                
+                else if(grade > 0 && grade <= 3)
+                {
+                    if (score >= 60)
+                    {
+                        Console.WriteLine($"입력하신 점수 {score}은(는) 합격입니다.");
+                    }
+                    else if (score < 60)
+                    {
+                        Console.WriteLine($"입력하신 점수 {score}은(는) 불합격입니다.");
+                    }
+                } 
+            }
+
+            else
+            {
+                Console.WriteLine("점수를 잘못 입력하셨습니다. 확인후 다시 입력해주세요");
+            }
             
-            int a = 1;
-            int b = 2;
-            int c = 3;
-            int d = 4;
-
-            Console.ReadLine();
-           
-            Console.WriteLine("현재 본인의 점수를 입력해 주세요.(ex.85):");
-            Console.ReadLine();
-            int e
-
-
-
-
         }
     }
 }
+
--- p151_ex6/Properties/AssemblyInfo.cs
+++ p151_ex6/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("dbd145c6-c214-482d-bead-4ac6ceeb634f")]
+[assembly: Guid("87e8e7cf-eea5-4dab-8ded-9c3e2575f518")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //

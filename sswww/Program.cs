4f46a92db78b33a1c84f45bb8af1e5e062ea622c
어진 김 <k91897599@gmail.com>
2023-03-16 15:48:02 +0900

    00

---
+++ sswww/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sswww
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] score = new int[] {0,1,2,3,4};
+            
+            foreach(int a in arr)
+            {
+                Console.WriteLine(a);
+
+            }
+        }
+    }
+}
